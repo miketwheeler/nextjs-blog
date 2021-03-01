7e7d7a002f141007c1a9709bbed576a480ef4858
miketwheeler <mikeytwheeler@gmail.com>
2021-03-01 15:56:53 -0800

    made changes to the overall layout

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,12 @@ import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <nav>
-        <ul>
+    <div className="mx-auto w-9/12 my-8">
+      <header>
+        <h1 className="text-6xl font-bold text-center">My Blog</h1>
+      </header>
+      <nav className="my-4">
+        <ul className="flex flex-row justify-center space-x-4">
           <li>
             <Link href="/">
               <a>Home</a>
